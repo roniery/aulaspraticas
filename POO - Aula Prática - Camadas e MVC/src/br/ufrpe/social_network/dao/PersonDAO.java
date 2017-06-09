47e17a577b66826acde9a89c2c30e613dcf102a5
roniery <roniery.araujo@hotmail.com>
2017-06-09 15:51:00 -0300

    Adicionada o metodo find e resta terminar o remove

--- POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/dao/PersonDAO.java
+++ POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/dao/PersonDAO.java
@@ -15,11 +15,33 @@ public class PersonDAO {
 		this.next = this.next + 1;
 	}
 	
-	public Person find(int id){
+	public Person find(long id){
+		
 		int i=0;
-		while
+		boolean achar = false;
+		
+		while((achar != true) && (i<this.next)){
+			if(id == this.person[i].getId()){
+				return this.person[i];
+				achar = true;
+				
+			}else{
+				i++;
+			}
+		}
 		
 	}
+	
+	public void delete(long id){
+		long encontrado = this.person.find(id);
+		
+		if(){
+		
+		}
+		
+	}
+	
+	
     //TODO implementar singleton para esta classe - PersonDAO
     
     //TODO Implementar CRUD para classe básica Person usando array
