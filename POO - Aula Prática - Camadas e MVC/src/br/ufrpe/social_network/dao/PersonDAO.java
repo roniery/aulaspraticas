d0fc1016172838a9cc5fcc892554f79e2b0e7160
roniery <roniery.araujo@homail.com>
2017-06-09 15:15:07 -0300

    Alterada o DAO DE person

--- POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/dao/PersonDAO.java
+++ POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/dao/PersonDAO.java
@@ -2,6 +2,24 @@ package br.ufrpe.social_network.dao;
 
 public class PersonDAO {
     
+	private Person[] person;
+	private int next;
+	
+	public PersonDAO(int tamanho){
+		this.person= new Person[tamanho];
+		this.next=0;
+	}
+	
+	public void cadastrar(Person p){
+		this.person[this.next]= p;
+		this.next = this.next + 1;
+	}
+	
+	public Person find(int id){
+		int i=0;
+		while
+		
+	}
     //TODO implementar singleton para esta classe - PersonDAO
     
     //TODO Implementar CRUD para classe básica Person usando array
